0c77ebd37a8252d98a5db4532ea13623f4e86c8c
SathanaP <you@example.com>
2026-05-30 19:11:30 +0530

    Enhanced sidebar UI with icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 
+import {
+  LayoutDashboard,
+  CheckSquare,
+  Timer,
+  BarChart3,
+} from "lucide-react";
+
 function Sidebar() {
   return (
     <div className="w-64 h-screen bg-zinc-950 border-r border-zinc-800 text-white p-5">
-      <h1 className="text-2xl font-bold mb-10">
+      <h1 className="text-3xl font-bold mb-10 text-blue-400">
         StudyFlow AI
       </h1>
 
-      <ul className="space-y-4">
+      <ul className="space-y-5">
+
         <li>
           <Link
             to="/"
-            className="hover:text-blue-400 hover:translate-x-2 transition duration-300 block"
+            className="flex items-center gap-3 hover:text-blue-400 hover:translate-x-2 transition duration-300"
           >
+            <LayoutDashboard size={20} />
             Dashboard
           </Link>
         </li>
@@ -20,8 +29,9 @@ function Sidebar() {
         <li>
           <Link
             to="/tasks"
-            className="hover:text-blue-400 hover:translate-x-2 transition duration-300 block"
+            className="flex items-center gap-3 hover:text-blue-400 hover:translate-x-2 transition duration-300"
           >
+            <CheckSquare size={20} />
             Tasks
           </Link>
         </li>
@@ -29,19 +39,23 @@ function Sidebar() {
         <li>
           <Link
             to="/timer"
-            className="hover:text-blue-400 hover:translate-x-2 transition duration-300 block"
+            className="flex items-center gap-3 hover:text-blue-400 hover:translate-x-2 transition duration-300"
           >
+            <Timer size={20} />
             Pomodoro Timer
           </Link>
         </li>
+
         <li>
           <Link
             to="/analytics"
-            className="hover:text-blue-400 hover:translate-x-2 transition duration-300 block"
+            className="flex items-center gap-3 hover:text-blue-400 hover:translate-x-2 transition duration-300"
           >
+            <BarChart3 size={20} />
             Analytics
           </Link>
         </li>
+
       </ul>
     </div>
   );
